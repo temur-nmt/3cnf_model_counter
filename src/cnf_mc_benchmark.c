d98ba1ab1cc8b1e0717cfde2cb084ae907812b34
temur-nmt <temur.nmt@gmail.com>
2026-06-12 15:01:58 +0200

    placeholder files

---
+++ src/cnf_mc_benchmark.c
@@ -0,0 +1,13 @@
+/*
+    TODO: create benchmark with sequential and parallel implementationf for the OMICS Cluster
+
+    1. finish sequential code
+    2. finish parallel code
+    3. finish benchmark
+    4. edit makefile
+
+    Idea:
+        - use the already provided benchmark files as template
+        - no need for a generator as we have pregenerated test cases
+            - add switch to generate new test cases if possible (requires the omics cluster to have python)
+*/
